fe938a69f4fcd9e8ac4461ae27dd36f8b0b8f3ad
yohrran <johnyohan93@gmail.com>
2021-06-05 02:29:43 +0900

    variablestudy

--- miniproject/Calendar/index.js
+++ miniproject/Calendar/index.js
@@ -1,3 +1,4 @@
+"use strict";
 const YEAR = document.querySelector(".year"),
   MONTH = document.querySelector(".month"),
   DATE = document.querySelector(".date"),
@@ -31,7 +32,6 @@ const monthEn = [
 
 function weekDay() {
   let i = 0;
-  let j = 0;
   if (date[i] === week[i]) {
     return;
   }
@@ -46,32 +46,51 @@ function miniToday() {
 
 function makeCalendar() {
   let row = CALENDAR.insertRow();
-  for (let i = 0; i < first_day; i++) {
-    let cell = row.insertCell();
+  let i;
+  let cells;
+  for (i = 0; i < first_day; i++) {
+    row.insertCell();
   }
   for (i = 1; i <= last_date; i++) {
     if (first_day != 7) {
-      cell = row.insertCell();
-      cell.setAttribute("id", [i]);
-      cell.innerHTML = [i];
+      cells = row.insertCell();
+      cells.setAttribute("id", i);
+      cells.innerHTML = i;
       first_day += 1;
     } else {
       row = CALENDAR.insertRow();
-      cell = row.insertCell();
-      cell.setAttribute("id", [i]);
-      cell.innerHTML = [i];
+      cells = row.insertCell();
+      cells.setAttribute("id", i);
+      cells.innerHTML = i;
       first_day -= 6;
+      //지역변수는 괄호안에서만 쓰인다.
     }
   }
 }
+
 function todayColor() {
   makeCalendar();
-  for (i = 1; i <= last_date; i++) {}
+  const sets = Array.from(document.querySelectorAll("td"));
+
+  //sets 안에 td가 들었죠 td 안에 들어있는 숫자.
+  //i는 숫자다 숫자랑 배열이랑 비교가 안된다. nodeList
+  //는 유사배열인데 배열이 아니다.
+  //배열로 다 하나씩 바꿔서 넣어서 그 배열을 이용해 [i]와
+  //date와 같은것에 색을 칠한다.
+  sets.forEach((j) => {
+    console.log(j.innerText); //innerText를 가져올 수 있다.
+    console.log(date);
+    if (String(date) === j.innerText) {
+      //타입을 비교한다 === / ==
+      //변수 안에 뭐가 들어있는지 보기
+      j.style.color = "red";
+    }
+  });
 }
 
 function init() {
   miniToday();
-  makeCalendar();
+  todayColor();
 }
 
 init();
